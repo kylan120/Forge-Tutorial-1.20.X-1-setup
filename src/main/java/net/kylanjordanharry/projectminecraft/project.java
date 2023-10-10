3b1304b027613d7236b916c151221c09e96bbc20
kylan120 <larsonkylan@gmail.com>
2023-10-10 17:48:59 -0500

    I added our faces as well as fixed the size of the images to make it more clear

--- src/main/java/net/kylanjordanharry/projectminecraft/project.java
+++ src/main/java/net/kylanjordanharry/projectminecraft/project.java
@@ -1,9 +1,10 @@
 package net.kylanjordanharry.projectminecraft;
-
 import com.mojang.logging.LogUtils;
+import net.kylanjordanharry.projectminecraft.item.ModFoods;
 import net.kylanjordanharry.projectminecraft.item.ModItems;
 import net.kylanjordanharry.projectminecraft.block.FaceBlocks;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -15,11 +16,15 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
+
+import java.util.function.Supplier;
+
 //Name: Kylan, Harry, and Jordan
 // Description: This is the project class this is our main program that loads everything in minecraft
 // Date: 9/28/2023
 @Mod(project.MOD_ID)
-public class project {
+public class project
+{
     public static final String MOD_ID = "project";
     public static final Logger LOGGER = LogUtils.getLogger();
 
@@ -29,23 +34,30 @@ public class project {
         ModItems.register(modEventBus); // This will make sure our items are added to the game
         FaceBlocks.register(modEventBus); // This will make sure our blocks are added to the game
         modEventBus.addListener(this::commonSetup);
-
         MinecraftForge.EVENT_BUS.register(this);
         modEventBus.addListener(this::addCreative);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
 
     }
 
-    // Add the example block item to the building blocks tab
+
+  // This adds the items to creative
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+
+
+
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS)
         {
             event.accept(ModItems.KYLAN);
             event.accept(ModItems.JORDAN);
             event.accept(ModItems.HARRY);
+            event.accept(ModItems.KYLANFOOD);
+
+
         }
         if(event.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS)
         {
@@ -61,6 +73,7 @@ public class project {
             event.accept(FaceBlocks.KYLAN_UBER);
             event.accept(FaceBlocks.JORDAN_UBER);
             event.accept(FaceBlocks.HARRY_UBER);
+
         }
         if(event.getTabKey() == CreativeModeTabs.COMBAT)
         {
@@ -73,14 +86,17 @@ public class project {
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
+    public void onServerStarting(ServerStartingEvent event)
+    {
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
+    public static class ClientModEvents
+    {
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
 
         }
     }
