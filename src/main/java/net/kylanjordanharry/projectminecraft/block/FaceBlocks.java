ccb221bcf5f7404abe6d9697fce045354d4eeda7
AncientTangeloes <119451157+AncientTangeloes@users.noreply.github.com>
2023-10-11 09:12:27 -0500

    Set block instruments
    Also set mining properties of ubers and fixed armor texture issue

--- src/main/java/net/kylanjordanharry/projectminecraft/block/FaceBlocks.java
+++ src/main/java/net/kylanjordanharry/projectminecraft/block/FaceBlocks.java
@@ -10,6 +10,7 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
@@ -25,55 +26,61 @@ public class FaceBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, project.MOD_ID);
 
     public static final RegistryObject<Block> KYLAN_BLOCK = registerBlock("kylan_block",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.SLIME_BLOCK)));
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.SLIME_BLOCK)
+                    .instrument(NoteBlockInstrument.WITHER_SKELETON)));
     public static final RegistryObject<Block> KYLAN_UBER = registerBlock("kylan_uber",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.SCULK_SENSOR)));
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.SCULK_SENSOR)
+                    .instrument(NoteBlockInstrument.DRAGON)));
     public static final RegistryObject<Block> KYLAN_ORE = registerBlock("kylan_ore",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.COPPER_ORE)
-                    .sound(SoundType.SLIME_BLOCK).requiresCorrectToolForDrops(),
-                    UniformInt.of(6, 9)));
+                    .sound(SoundType.SLIME_BLOCK).instrument(NoteBlockInstrument.SKELETON)
+                    .requiresCorrectToolForDrops(), UniformInt.of(6, 9)));
     public static final RegistryObject<Block> KYLAN_ORE_DEEPSLATE = registerBlock("kylan_ore_deepslate",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE_COPPER_ORE)
-                    .sound(SoundType.SLIME_BLOCK).requiresCorrectToolForDrops(),
-                    UniformInt.of(6, 9)));
+                    .sound(SoundType.SLIME_BLOCK).instrument(NoteBlockInstrument.SKELETON)
+                    .requiresCorrectToolForDrops(), UniformInt.of(6, 9)));
     public static final RegistryObject<Block> KYLAN_ORE_NETHER = registerBlock("kylan_ore_nether",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.NETHER_QUARTZ_ORE)
-                    .sound(SoundType.SLIME_BLOCK).requiresCorrectToolForDrops(),
-                    UniformInt.of(6, 9)));
+                    .sound(SoundType.SLIME_BLOCK).instrument(NoteBlockInstrument.SKELETON)
+                    .requiresCorrectToolForDrops(), UniformInt.of(6, 9)));
 
     public static final RegistryObject<Block> JORDAN_BLOCK = registerBlock("jordan_block",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.STEM)));
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.STEM)
+                    .instrument(NoteBlockInstrument.WITHER_SKELETON)));
     public static final RegistryObject<Block> JORDAN_UBER = registerBlock("jordan_uber",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.SCULK_SENSOR)));
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.SCULK_SENSOR)
+                    .instrument(NoteBlockInstrument.DRAGON)));
     public static final RegistryObject<Block> JORDAN_ORE = registerBlock("jordan_ore",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.COPPER_ORE)
-                    .sound(SoundType.STEM).requiresCorrectToolForDrops(),
+                    .sound(SoundType.STEM).instrument(NoteBlockInstrument.SKELETON).requiresCorrectToolForDrops(),
                     UniformInt.of(6, 9)));
     public static final RegistryObject<Block> JORDAN_ORE_DEEPSLATE = registerBlock("jordan_ore_deepslate",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE_COPPER_ORE)
-                    .sound(SoundType.STEM).requiresCorrectToolForDrops(),
+                    .sound(SoundType.STEM).instrument(NoteBlockInstrument.SKELETON).requiresCorrectToolForDrops(),
                     UniformInt.of(6, 9)));
     public static final RegistryObject<Block> JORDAN_ORE_NETHER = registerBlock("jordan_ore_nether",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.NETHER_QUARTZ_ORE)
-                    .sound(SoundType.STEM).requiresCorrectToolForDrops(),
+                    .sound(SoundType.STEM).instrument(NoteBlockInstrument.SKELETON).requiresCorrectToolForDrops(),
                     UniformInt.of(6, 9)));
 
     public static final RegistryObject<Block> HARRY_BLOCK = registerBlock("harry_block",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.HONEY_BLOCK)));
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.HONEY_BLOCK)
+                    .instrument(NoteBlockInstrument.WITHER_SKELETON)));
     public static final RegistryObject<Block> HARRY_UBER = registerBlock("harry_uber",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.SCULK_SENSOR)));
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.SCULK_SENSOR)
+                    .instrument(NoteBlockInstrument.DRAGON)));
     public static final RegistryObject<Block> HARRY_ORE = registerBlock("harry_ore",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.COPPER_ORE)
-                    .sound(SoundType.HONEY_BLOCK).requiresCorrectToolForDrops(),
-                    UniformInt.of(6, 9)));
+                    .sound(SoundType.HONEY_BLOCK).instrument(NoteBlockInstrument.SKELETON)
+                    .requiresCorrectToolForDrops(), UniformInt.of(6, 9)));
     public static final RegistryObject<Block> HARRY_ORE_DEEPSLATE = registerBlock("harry_ore_deepslate",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE_COPPER_ORE)
-                    .sound(SoundType.HONEY_BLOCK).requiresCorrectToolForDrops(),
-                    UniformInt.of(6, 9)));
+                    .sound(SoundType.HONEY_BLOCK).instrument(NoteBlockInstrument.SKELETON)
+                    .requiresCorrectToolForDrops(), UniformInt.of(6, 9)));
     public static final RegistryObject<Block> HARRY_ORE_NETHER = registerBlock("harry_ore_nether",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.NETHER_QUARTZ_ORE)
-                    .sound(SoundType.HONEY_BLOCK).requiresCorrectToolForDrops(),
-                    UniformInt.of(6, 9)));
+                    .sound(SoundType.HONEY_BLOCK).instrument(NoteBlockInstrument.SKELETON)
+                    .requiresCorrectToolForDrops(), UniformInt.of(6, 9)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
