0299094956964280f7fda5284efd99a6dec33a91
kylan120 <larsonkylan@gmail.com>
2023-10-11 14:30:51 -0500

    Deleted placeholder

--- src/main/java/net/kylanjordanharry/projectminecraft/project.java
+++ src/main/java/net/kylanjordanharry/projectminecraft/project.java
@@ -32,6 +32,7 @@ public class project
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
         modEventBus.addListener(this::addCreative);
+        ;
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
